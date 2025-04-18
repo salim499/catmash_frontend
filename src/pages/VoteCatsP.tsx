48aa0471eadf225505a0b8cc9cb36552b00d87fa
Salim Hassouna <hassounasalim842@gmail.com>
2025-04-18 20:42:22 +0200

    feat: implement initial voting logic with two cats in VoteCats

--- src/pages/VoteCatsP.tsx
+++ src/pages/VoteCatsP.tsx
@@ -1,5 +1,50 @@
+// Import styles
+import { DivVoteContainerS, VSBadgeS } from "../styles/VoteCatsP.style";
+
+// Import constants
+import { left, right } from "../constants";
+
+// Import components
+import FigureC from "../components/FigureC";
+
 const VoteCatsP = () => {
-  return <div>VoteCatsP</div>;
+  const handleVoteCat = async (direction: string) => {};
+
+  return (
+    <>
+      {/* Container to hold the two cats and voting logic */}
+      <DivVoteContainerS>
+        {/* Display the left cat if available */}
+
+        <FigureC
+          handleVoteCat={() => handleVoteCat(left)}
+          cat={{
+            id: "MTgwODA3MA",
+            name: "chat 1",
+            url: "http://24.media.tumblr.com/tumblr_m82woaL5AD1rro1o5o1_1280.jpg",
+            score: 0,
+          }}
+          rotateDirection={left}
+        />
+
+        {/* Display the "VS" badge between the two cats */}
+        <VSBadgeS>VS</VSBadgeS>
+
+        {/* Display the right cat if available */}
+
+        <FigureC
+          handleVoteCat={() => handleVoteCat(right)}
+          cat={{
+            id: "tt",
+            name: "chat 2",
+            url: "http://24.media.tumblr.com/tumblr_m29a9d62C81r2rj8po1_500.jpg",
+            score: 0,
+          }}
+          rotateDirection={right}
+        />
+      </DivVoteContainerS>
+    </>
+  );
 };
 
 export default VoteCatsP;
---
+++ src/styles/VoteCatsP.style.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const DivVoteContainerS = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 40px;
+  margin-top: 50px;
+`;
+
+export const VSBadgeS = styled.p`
+  font-size: 2rem;
+  font-weight: bold;
+  font-style: italic;
+  padding: 10px 10px;
+  border-radius: 50%;
+  color: rgba(47, 85, 150, 1);
+  border: 2px solid rgba(47, 85, 150, 1);
+  color: rgba(47, 85, 150, 1);
+`;
