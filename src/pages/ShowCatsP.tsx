b32bac9b0ed5f8595ed2db38b9b5dc65337b11f8
Salim Hassouna <hassounasalim842@gmail.com>
2025-04-18 20:08:34 +0200

    feat: display paginated cats with FigureC component

--- src/pages/ShowCatsP.tsx
+++ src/pages/ShowCatsP.tsx
@@ -4,36 +4,53 @@ import { useState, useEffect } from "react";
 // Import SWR for api interactions
 import { mutate } from "swr";
 
+// Import styles
+import { DivShowCatsContainerS } from "../styles/ShowCatsP.style";
+
+// Import types
+import { CatT } from "../types/CatT";
+
 // Import components
+import FigureC from "../components/FigureC";
 import LoaderC from "../components/LoaderC";
 import ErrorC from "../components/ErrorC";
 
 // Import custom hooks
 import UseGetCats from "../hooks/UseGetCats";
+import UseIsScreenWidthLessOrEqual from "../hooks/UseIsScreenWidthLessOrEqual";
 
 const ShowCatsP = () => {
   // State to manage the current pagination
   const [page, setPage] = useState(1);
+
+  // Check if the screen width is less than or equal to 1024px
+  const isScreenWidthLessOrEqual = UseIsScreenWidthLessOrEqual(1024);
 
   // Fetch the list of cats with pagination (9 cats per page)
   const { cats, error, isLoading } = UseGetCats({
     limit: 9,
     offset: (page - 1) * 9,
   });
-
-  // When the pagination changes, update the SWR cache (forces data reload)
-  useEffect(() => {
-    mutate({ limit: 10, offset: page - 1 });
-  }, [page]);
-
-  // If data is still loading
-  if (isLoading) return <LoaderC />;
-
-  // If there is error
-  if (error) return <ErrorC />;
+  console.log(error);
 
   // If everything is fine
-  return <>no error</>;
+  return (
+    <>
+      {/* Main container for the cats cards */}
+      <DivShowCatsContainerS>
+        {cats &&
+          cats.map((cat: CatT, index: number) => (
+            <FigureC
+              key={cat.id}
+              index={index}
+              page={page}
+              isScreenWidthLessOrEqual={isScreenWidthLessOrEqual}
+              cat={cat}
+            />
+          ))}
+      </DivShowCatsContainerS>
+    </>
+  );
 };
 
 export default ShowCatsP;
---
+++ src/styles/ShowCatsP.style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const DivShowCatsContainerS = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: end;
+  gap: 20px;
+  margin-top: 50px;
+`;
